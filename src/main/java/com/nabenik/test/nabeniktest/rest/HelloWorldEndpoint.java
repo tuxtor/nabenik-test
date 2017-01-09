0e5971ac728a1d444657a4c4f7caa3c35cc90448
Esvux <usac.ing.esvin@gmail.com>
2017-01-09 05:15:38 -0600

    Validación de parámetros lista

--- src/main/java/com/nabenik/test/nabeniktest/rest/HelloWorldEndpoint.java
+++ src/main/java/com/nabenik/test/nabeniktest/rest/HelloWorldEndpoint.java
@@ -1,6 +1,6 @@
 package com.nabenik.test.nabeniktest.rest;
 
-import java.util.ArrayList;
+import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import com.nabenik.test.nabeniktest.controller.MD5EncryptedService;
@@ -39,6 +40,28 @@ public class HelloWorldEndpoint {
 	@Path("/check-polo")
 	@Produces("application/json")
 	public List<Integer> checkMarcoPolo(@QueryParam("marco") String marco, @QueryParam("polo") String polo, @QueryParam("md5sum") String md5sum) {
+		if(marco==null){
+			throw new WebApplicationException(
+					Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+					.entity("El parámetro 'marco' es obligatorio.")
+					.build()
+			);
+		}
+		if(polo==null){
+			throw new WebApplicationException(
+					Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+					.entity("El parámetro 'polo' es obligatorio.")
+					.build()
+			);
+		}
+		if(md5sum==null){
+			throw new WebApplicationException(
+					Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+					.entity("El parámetro 'md5' es obligatorio.")
+					.build()
+			);
+		}
+		//Parámetros correctos...
 		List<Integer> list = randomNumberService.generateNumbers(); 
 		String anotherMD5 = md5EncryptedService.generateMD5(marco + polo);
 		if(anotherMD5.compareTo(md5sum)==0){
