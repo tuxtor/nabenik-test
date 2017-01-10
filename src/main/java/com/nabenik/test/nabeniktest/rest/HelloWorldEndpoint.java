aa8ff6df141cea25510b03a22d508d0062dda22b
otrebmer <jose.usac.edu@gmail.com>
2017-01-10 17:18:46 -0600

    Add files via upload

--- src/main/java/com/nabenik/test/nabeniktest/rest/HelloWorldEndpoint.java
+++ src/main/java/com/nabenik/test/nabeniktest/rest/HelloWorldEndpoint.java
@@ -1,5 +1,9 @@
 package com.nabenik.test.nabeniktest.rest;
 
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +12,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 import com.nabenik.test.nabeniktest.controller.RandomNumberService;
@@ -35,14 +40,52 @@ public class HelloWorldEndpoint {
 	@Produces("application/json")
 	public List<Integer> checkMarcoPolo(@QueryParam("marco") String marco, @QueryParam("polo") String polo, @QueryParam("md5sum") String md5sum) {
 		
-		if(true){
+		if (marco == null) {
+		    throw new WebApplicationException(
+		      Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+		        .entity("marco parameter is mandatory")
+		        .build()
+		    );
+		  }
+		  if (polo == null) {
+		    throw new WebApplicationException(
+		      Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+		        .entity("polo parameter is mandatory")
+		        .build()
+		    );
+		  }
+		  if (md5sum == null) {
+		    throw new WebApplicationException(
+		      Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+		        .entity("polo parameter is mandatory")
+		        .build()
+		    );
+		  }
+
+		
+		if(md5sum.compareTo(md5(marco+polo))==0){
 			return randomNumberService.generateNumbers();
 		}else{
-			return new ArrayList<>();
+			return randomNumberService.generateNumbers2();
 		}
 		
 	}
 	
-	
+	public String md5(String input){
+		try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+            
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+	}
 	
 }
